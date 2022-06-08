f6276b934f853b055335b99e4a1e2b019ef19bd6
solutionsutras <89479220+solutionsutras@users.noreply.github.com>
2022-06-08 23:42:52 +0530

    third

--- app.js
+++ app.js
@@ -48,10 +48,7 @@ mongoose.connect(process.env.CONNECTION_STRING,{
 // })
 
 // Production
-// var server = app.listen(process.env.PORT || 3000, function(){
-//    var port = server.address().port;
-//    console.log("Server Running at port: " + port)
-// })
-app.listen(process.env.PORT || 3000, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
+	var server = app.listen(process.env.PORT || 3000, function(){
+    var port = server.address().port;
+    console.log("Server Running at port: " + port)
+})
